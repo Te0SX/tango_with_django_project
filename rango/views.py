a02c157627898207a4524d57dbd878fd44425c77
Theodoros Vrakas <te0sx@yahoo.gr>
2021-07-27 14:06:54 +0100

    Complete chapter 4 + exercise

--- rango/views.py
+++ rango/views.py
@@ -7,14 +7,21 @@ def index(request):
 	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 
 	# Return a rendered response to send to the client.
-	# We make use of the shortcut function to make our lives easier. # Note that the first parameter is the template we wish to use. 
 	return render(request, 'rango/index.html', context=context_dict)
 
-	
-# Part of the Exercise 3
 def about(request):
-	return HttpResponse("Rango says here is the about page. " + "<a href='/rango/'>Index</a>")
+	# Construct a dictionary to pass to the template engine as its context.
+	# Note the key boldmessage matches to {{ boldmessage }} in the template! 
+	context_dict = {'boldmessage': 'This tutorial has been put together by Theodoros Vrakas'}
+
+	# Return a rendered response to send to the client.
+	return render(request, 'rango/about.html', context=context_dict)
+	
 
 
 # def index(request):
 # 	return HttpResponse("Rango says hey there partner! " + "<a href='/rango/about/'>About</a>")
+
+# # Part of the Exercise 3
+# def about(request):
+# 	return HttpResponse("Rango says here is the about page. " + "<a href='/rango/'>Index</a>")
